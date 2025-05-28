96b1f64c4e4d60d9a99be01a1017377d04c29901
VRReddy123 <vijayragava2004@gmail.com>
2025-05-28 15:18:21 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,12 @@
 import './App.css'
 import React from 'react';
-import Counter from './Module 3/React Hooks - useState & useEffect/Ques_1_Counter.jsx';
+import TextDisplay from './Module 3/React Hooks - useState & useEffect/Ques_2_Text_Input_and_Display.jsx';
 function App() {
 
   return (
     <>
       {/* Add your component here */}
-       <Counter />
+       <TextDisplay />
     </>
   )
 }
--- src/Module 3/React Hooks - useState & useEffect/Ques_2_Text_Input_and_Display.jsx
+++ src/Module 3/React Hooks - useState & useEffect/Ques_2_Text_Input_and_Display.jsx
@@ -3,3 +3,27 @@
 
 // Steps:
 //     - Write your code within the file, by the name of component as Text_Input
+
+import React, { useState } from 'react';
+
+function TextDisplay() {
+  const [text, setText] = useState('');
+
+  const handleChange = (event) => {
+    setText(event.target.value);
+  };
+
+  return (
+    <div>
+      <input
+        type="text"
+        value={text}
+        onChange={handleChange}
+        placeholder="Enter text here"
+      />
+      <p>You entered: {text}</p>
+    </div>
+  );
+}
+
+export default TextDisplay;
