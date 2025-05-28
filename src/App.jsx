f2fe10388f2131064c124bb91981f77bbaa27ee7
VRReddy123 <vijayragava2004@gmail.com>
2025-05-28 15:05:41 +0000

    done

--- src/App.jsx
+++ src/App.jsx
@@ -1,29 +1,28 @@
-import './App.css'
+import './App.css';
 import React, { useState } from 'react';
-function App() {
-
-  return (
-    <>
-      {/* Add your component here */
 
 function Counter() {
-  const [count, setCount] = useState(0);
+    const [count, setCount] = useState(0);
 
-  const increment = () => {
-    setCount(count + 1);
-  };
+    const increment = () => {
+        setCount(count + 1);
+    };
 
-  return (
-    <div>
-      <h1>Counter: {count}</h1>
-      <button onClick={increment}>Increment</button>
-    </div>
-  );
-}}
-export default Counter;
-  
-    </>
-  )
+    return (
+        <div>
+            <h1>Counter: {count}</h1>
+            <button onClick={increment}>Increment</button>
+        </div>
+    );
 }
 
-export default App
+function App() {
+    return (
+        <>
+            {/* Add your component here */}
+            <Counter />
+        </>
+    );
+}
+
+export default App;
